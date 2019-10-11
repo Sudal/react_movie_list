6bae774b4028f2603071f3e99ba8a01ec5f16f72
yjo0512 <yjo0512@midasit.com>
2019-10-11 10:36:38 +0900

    Updates

---
+++ precache-manifest.39a58070f972a8fac1c566eaea40a795.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f0a082b9a2386cca3ba5affe6a25948",
+    "url": "/react_movie_list/index.html"
+  },
+  {
+    "revision": "9ff40b5d916fd788c4bf",
+    "url": "/react_movie_list/static/css/main.ae4cfe19.chunk.css"
+  },
+  {
+    "revision": "b00a8793e10818db85ce",
+    "url": "/react_movie_list/static/js/2.811080b3.chunk.js"
+  },
+  {
+    "revision": "9ff40b5d916fd788c4bf",
+    "url": "/react_movie_list/static/js/main.e69a9e49.chunk.js"
+  },
+  {
+    "revision": "07bc0411f95306a3a1d7",
+    "url": "/react_movie_list/static/js/runtime~main.a66ecbc2.js"
+  }
+]);
